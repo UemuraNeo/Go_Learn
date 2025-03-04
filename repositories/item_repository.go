a3f9648a71ab745034cc0b671b78260ab5206e3b
UemuraNeo <ikumi.uemura@neosystem.co.jp>
2025-03-04 10:24:14 +0900

    Read & Create操作の実装

---
+++ repositories/item_repository.go
@@ -0,0 +1,39 @@
+package repositories
+
+import (
+	"errors"
+	models "gin-test/models"
+)
+
+type IItemRepository interface {
+	FindAll() (*[]models.Item, error)
+	FindById(itemId uint) (*models.Item, error)
+	Create(newItem models.Item) (*models.Item, error)
+}
+
+type ItemMemoryRepository struct {
+	items []models.Item
+}
+
+func NewItemMemoryRepository(items []models.Item) IItemRepository {
+	return &ItemMemoryRepository{items: items} //interface型を戻すことで、実装がinterfaceの定義を満たしていない場合エラーが発生し未然に防げる
+}
+
+func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
+	return &r.items, nil
+}
+
+func (r *ItemMemoryRepository) FindById(itemId uint) (*models.Item, error) {
+	for _, v := range r.items {
+		if v.ID == itemId {
+			return &v, nil
+		}
+	}
+	return nil, errors.New("Item not found")
+}
+
+func (r *ItemMemoryRepository) Create(newItem models.Item) (*models.Item, error) {
+	newItem.ID = uint(len(r.items) + 1)
+	r.items = append(r.items, newItem)
+	return &newItem, nil
+}
