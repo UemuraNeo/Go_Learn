0561bc1f550b82422b2e08d0d178477bc9f27ad1
UemuraNeo <ikumi.uemura@neosystem.co.jp>
2025-03-05 10:32:24 +0900

    Update & Delete実装

--- main.go
+++ main.go
@@ -30,5 +30,7 @@ func main() {
 	r.GET("/items", itemController.FindAll) //関数そのものを渡す（）いらない
 	r.GET("/items/:id", itemController.FindById)
 	r.POST("/items", itemController.Create)
+	r.PUT("/items/:id", itemController.Update)
+	r.DELETE("/items/:id", itemController.Delete)
 	r.Run("localhost:8080") //３：サーバーのアドレスやポート番号を指定する
 }
